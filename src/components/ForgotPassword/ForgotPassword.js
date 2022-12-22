8aff84ae7289e99607bf653f400326ec5da68745
codernoyan <coder.noyan@gmail.com>
2022-12-22 11:44:08 +0600

    bugs fixed

--- src/components/ForgotPassword/ForgotPassword.js
+++ src/components/ForgotPassword/ForgotPassword.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ForgotPassword = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h2 className="text-4xl font-bold">Coming <span className="text-orange-500">Soon</span></h2>
+      <h2 className="text-4xl font-bold">Coming <span className="text-red-500">Soon</span></h2>
     </div>
   );
 };
